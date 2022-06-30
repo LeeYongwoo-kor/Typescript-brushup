ce3c48be2c1dc5619cbe3bbf518ec9b99dd63c4e
LeeYongwoo-kor <newlyo1205@naver.com>
2022-07-01 00:10:38 +0900

    Fix generics & call signatures

--- last&prepend.ts
+++ last&prepend.ts
@@ -1,12 +1,9 @@
 type Last = <T>(a: T[]) => T;
-type Prepend = <T, M>(a: T[], b: M) => T;
+type Prepend = <T, M>(a: T[], b: M) => (T | M)[];
 
-const last: Last = (arr) => {
-  return arr[arr.length - 1];
-};
+const last: Last = (arr) => arr[arr.length - 1];
 
-const prepend: Prepend = (arr, item) => {
-  return [item, ...arr];
-};
+const prepend: Prepend = (arr, item) => [item, ...arr];
 
 console.log(last([1, 2, 3, 4]));
+console.log(prepend([1, 2, 3, 4], 5));
