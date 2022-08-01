400a7af02a6512079075fd4593357c6610c7e17f
LeeYongwoo-kor <newlyo1205@naver.com>
2022-08-01 21:54:04 +0900

    Add generic2(Constraints, Conditional Types, Infer)

--- Functions/generic2.ts
+++ Functions/generic2.ts
@@ -61,3 +61,47 @@ function wrap<T>(param: T) {
 }
 
 const wrapped = wrap(10);
+
+// Constraints
+type U = string | number | boolean;
+
+// type 식별자 = 타입 구현
+type MyType<T extends U> = string | T;
+
+// interface 식별자 { 타입 구현 }
+interface IUser<T extends U> {
+  name: string;
+  age: T;
+}
+
+// Conditional Types
+// `T`는 `boolean` 타입으로 제한.
+interface IUser2<T extends boolean> {
+  name: string;
+  age: T extends true ? string : number; // `T`의 타입이 `true`인 경우 `string` 반환, 아닌 경우 `number` 반환.
+  isString: T;
+}
+
+const str: IUser2<true> = {
+  name: "Neo",
+  age: "12", // String
+  isString: true,
+};
+const num: IUser2<false> = {
+  name: "Lewis",
+  age: 12, // Number
+  isString: false,
+};
+
+// infer
+type TReturnType<T extends (...args: any) => any> = T extends (
+  ...args: any
+) => infer R
+  ? R
+  : any;
+
+function fn(num: number) {
+  return num.toString();
+}
+
+const a: ReturnType<typeof fn> = "Hello";
