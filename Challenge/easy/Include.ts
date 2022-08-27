f546cbae4e803460d3e8a6516a38338abe2599c2
LeeYongwoo-kor <newlyo1205@naver.com>
2022-08-27 19:17:33 +0900

    Add TS Challenge 4 Problems

---
+++ Challenge/easy/Include.ts
@@ -0,0 +1,7 @@
+type Includes<T extends readonly any[], U> = T extends [infer F, ...infer R]
+  ? Equal<F, U> extends true
+    ? true
+    : Includes<R, U>
+  : false;
+
+type isPillarMen = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Dio">; // expected to be `false`
---
+++ Challenge/easy/concat.ts
@@ -0,0 +1,3 @@
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
+
+type Result = Concat<[1], [2]>; // expected to be [1, 2]
